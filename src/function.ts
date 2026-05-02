c742b86d9e009bf11921b7ebe1c074755383db19
slaman-farcy <salmanfarcy253@gmail.com>
2026-05-02 08:13:14 +0600

    function and method defind type

---
+++ src/function.ts
@@ -0,0 +1,29 @@
+//function
+// arow function, normal function
+
+// function add(num1: number, num2: number) : number {
+//      return num1 + num2;
+// }
+// console.log(add(2, 2))
+
+// const addArrow = (num1: number, num2: number) : number => num1 + num2
+// console.log(addArrow(2, 2))
+
+
+// object => function => method 
+
+// const goribUser = { 
+//      name : "salman",
+//      balance : 5,
+//      addBalance(value: number) : number {
+//           const totalBalance = this.balance + value;
+//           return totalBalance;
+//      }
+// } 
+
+// console.log(goribUser.addBalance(100))
+
+
+// const arr : number[] = [1, 2, 3, 4, 5];
+
+// const squerArray = arr.map((em : number) : number => em * em); 
