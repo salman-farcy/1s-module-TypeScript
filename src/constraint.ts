a68f33bcca85727da3e26b8347a82054d69668ba
slaman-farcy <salmanfarcy253@gmail.com>
2026-05-05 19:19:58 +0600

    keyOf constraint

--- src/constraint.ts
+++ src/constraint.ts
@@ -1,25 +1,41 @@
 // constraint : strict rules deya
 
-type Student = {
-     id: number, 
-     name: string,
-     email: string,
-     userNmae: string
-}
-const addStudentToCourse = <T extends Student> (studentInfo : T) => {
+// type Student = {
+//      id: number, 
+//      name: string,
+//      email: string,
+//      userNmae: string
+// }
+// const addStudentToCourse = <T extends Student> (studentInfo : T) => {
+//      return {
+//           course: "Next Level Web Development",
+//           ...studentInfo
+//      }
+// }
+
+// const student5 = {
+//      id: 5,
+//      name: "John Doe",
+//      email: "john.doe@example.com",
+//      userNmae: "john_doe"
+// }
+
+
+// const student1 = addStudentToCourse(student5)
+// console.log(student1)
+
+type StudentMustRequered = {id: number, name: string, }
+
+const myGnericFunction = <T extends StudentMustRequered> (value : T) => {
      return {
-          course: "Next Level Web Development",
-          ...studentInfo
+          myWold : "Salman farcy wold in Ever"
      }
 }
 
 const student5 = {
-     id: 5,
-     name: "John Doe",
-     email: "john.doe@example.com",
-     userNmae: "john_doe"
+     id: 3420,
+     name: "Salman farcy",
+     hasPen: true
 }
 
-
-const student1 = addStudentToCourse(student5)
-console.log(student1)
+const student = myGnericFunction(student5)
